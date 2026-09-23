661974bb4f973172bb6a40039583481310569340
Stephen Blythe <sblythe@lindenwood.edu>
2026-09-23 12:52:47 -0500

    Added some basic bitwise operation code

--- BitWiseOps/bitops.cpp
+++ BitWiseOps/bitops.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#define NBITS sizeof(unsigned int)*8 
+
 using namespace std;
+
+
 
 void printBin(unsigned int num,
 	      unsigned int nbits,
@@ -28,12 +32,54 @@ int main (int argc, char *argv[])
   unsigned int val1 = stoi(argv[1]);
   unsigned int val2 = stoi(argv[2]);
 
-  printBin(val1, 32);
-  cout << endl;
-  printBin(val2, 32);
-  cout << endl;
-  
-  
+  cout << "Logical and (&&) gives ... "
+       << (val1 && val2) << endl;
 
+  cout << "Bitwise and (&) gives ... "
+       << (val1 & val2) << endl;
+  
+  printBin(val1, NBITS);
+  cout << endl;
+  printBin(val2, NBITS);
+  cout << endl;
+  printBin(val1&val2, NBITS);
+  cout << endl;
+
+  cout << "Logical or (||) gives ... "
+       << (val1 || val2) << endl;
+
+  cout << "Bitwise and (|) gives ... "
+       << (val1 | val2) << endl;
+  
+  printBin(val1, NBITS);
+  cout << endl;
+  printBin(val2, NBITS);
+  cout << endl;
+  printBin(val1|val2, NBITS);
+  cout << endl;
+
+  cout << "What bit position in " << val1 << " do you want to see? ";
+  unsigned int pos;
+  cin >> pos;
+
+  unsigned mask = 1;
+  mask = mask << pos;
+  printBin(val1, NBITS);
+  cout << endl;
+  printBin(mask, NBITS);
+  cout << endl;
+  printBin(mask& val1, NBITS);
+  cout << endl;
+
+  if (mask&val1)
+    cout << "it was a 1!" << endl;
+  else
+    cout << "it was a 0!" << endl;
+
+  printBin(val1, NBITS);
+  cout << endl;
+  printBin(~val1, NBITS);
+  cout << endl;
+  
   return 0;
 }
